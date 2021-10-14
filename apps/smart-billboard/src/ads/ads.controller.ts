e1a5161d400bfeca042f778eb0fd0e9a86212568
Aleksander Pietrzak <Aleksanderp1993@gmail.com>
2021-10-14 17:19:51 +0200

    wagger is not displaying ApiProperties of the cont (#5)

--- apps/smart-billboard/src/ads/ads.controller.ts
+++ apps/smart-billboard/src/ads/ads.controller.ts
@@ -1,5 +1,8 @@
 import { Body, Controller, Get, Param, ParseIntPipe, Post, Query } from '@nestjs/common';
+import { ApiResponse } from '@nestjs/swagger';
 import { AdsService } from './ads.service';
+import { AdStatusDto } from './dto/ad-status.dto';
+import { buyAdResultDto } from './dto/buy-ad-result.dto';
 import { BuyAdDto } from './dto/buy-ad.dto';
 
 @Controller('ads')
@@ -9,12 +12,14 @@ export class AdsController {
     ) { }
 
     @Post('/buyAd')
-    buyAd(@Body() buyAdDto: BuyAdDto) {
-        return this.adsService.buyAd(buyAdDto);
+    @ApiResponse({ status: 201, type: buyAdResultDto, description: 'returns buyAdResultDto TODO' })
+    async buyAd(@Body() buyAdDto: BuyAdDto) : Promise<buyAdResultDto> {
+        return await this.adsService.buyAd(buyAdDto);
     }
 
     @Get(':id')
-    adStatus(@Param('id', ParseIntPipe) id: number) {
-        return this.adsService.getAdStatus(id);
+    @ApiResponse({ status: 200, type: AdStatusDto, description: 'returns AdStatusDto TODO' })
+    async adStatus(@Param('id', ParseIntPipe) id: number) :Promise<AdStatusDto> {
+        return await this.adsService.getAdStatus(id);
     }
 }
